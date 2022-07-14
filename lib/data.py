add cache to loader + improve scaling + bug fix in scaling and plot drawing

lib/data.py
@@ -4,12 +4,44 @@ from sklearn.preprocessing import MinMaxScaler
 import torch, os, pickle
 
 from torch.utils import data
-from sklearn.model_selection import train_test_split
 from tqdm import tqdm
+from cachetools import cached, Cache
 
 DEFAULT_DATA_PATH = './stock_market_data'
-SCALER_PATH = './minmax_scaler.bin'
-VOLUME_SCALER_PATH = './volume_scaler.bin'
+SCALER_PATH = './scalers/minmax_scaler.bin'
+VOLUME_SCALER_PATH = './scalers/volume_scaler.bin'
+CACHE = Cache(maxsize=1000)
+
+
+MONEY_RANGE = [0.0, 8324640145408.0]
+ADJUSTED_RANGE = [-616456459764173628137646621458432.0, 942168706172424929960592932864.0]
+VOLUME_RANGE = [0.0 ,7421640800.0]
+
+def scale(data: torch.Tensor, new_min=0, new_max=1):
+    money = data[:, :4]
+    adj = data[:, 4:5]
+    volume = data[:, -1:]
+
+    money = (money - MONEY_RANGE[0]) / (MONEY_RANGE[1] - MONEY_RANGE[0]) * (new_max - new_min) + new_min
+    adj = (adj - ADJUSTED_RANGE[0]) / (ADJUSTED_RANGE[1] - ADJUSTED_RANGE[0]) * (new_max - new_min) + new_min
+    volume = (volume - VOLUME_RANGE[0]) / (VOLUME_RANGE[1] - VOLUME_RANGE[0]) * (new_max - new_min) + new_min
+    return torch.hstack((money, adj, volume))
+
+def inverse_scale(data: torch.Tensor, curr_max=1, with_vol: bool = False):
+    money = data[:, :4]
+    adj = data[:, 4:5]
+
+    money = money / curr_max * (MONEY_RANGE[1] - MONEY_RANGE[0]) + MONEY_RANGE[0]
+    adj = adj / curr_max * (ADJUSTED_RANGE[1] - ADJUSTED_RANGE[0]) + ADJUSTED_RANGE[0]
+
+    if with_vol:
+        volume = data[:, -1:]
+        volume = volume / curr_max * (VOLUME_RANGE[1] - VOLUME_RANGE[0]) + VOLUME_RANGE[0]
+
+        return torch.hstack((money, adj, volume))
+    return torch.hstack((money, adj))
+
+
 
 def get_all_csv_files(dir: str):
     files = []
@@ -25,34 +57,44 @@ def get_all_csv_files(dir: str):
 def large_df(files):
     return pd.concat((pd.read_csv(f) for i, f in tqdm(enumerate(files), total=len(files))), ignore_index=True)
 
-def make_scaler():
-    print('=> Making scaler from all files')
-    X = large_df(get_all_csv_files(DEFAULT_DATA_PATH))
-    X = X.dropna(axis=0)
-    X = X.drop(['Date'], axis=1)
-    X = X[['Low', 'Open', 'High', 'Close', 'Adjusted Close', 'Volume']]
-    X = X.to_numpy()
+# def make_scaler():
+#     print('=> Making scaler from all files')
+#     x = large_df(get_all_csv_files(DEFAULT_DATA_PATH))
+#     x = x.dropna(axis=0)
+#     x = x.drop(['Date'], axis=1)
+#     x = x[['Low', 'Open', 'High', 'Close', 'Adjusted Close', 'Volume']]
+#     x = x.to_numpy()
     
-    scl = MinMaxScaler(feature_range=(0, 1))
-    vscl = MinMaxScaler(feature_range=(0, 1))
+#     scl = MinMaxScaler(feature_range=(0, 1))
+#     vscl = MinMaxScaler(feature_range=(0, 1))
 
-    scl = scl.fit(X[:, -1:])
-    vscl = vscl.fit(X[:, :-1])
+#     scl = scl.fit(x[:, :-1])
+#     vscl = vscl.fit(x[:, -1:])
 
-    pickle.dump(scl, open(SCALER_PATH, 'wb'))
-    pickle.dump(vscl, open(VOLUME_SCALER_PATH, 'wb'))
+#     pickle.dump(scl, open(SCALER_PATH, 'wb'))
+#     pickle.dump(vscl, open(VOLUME_SCALER_PATH, 'wb'))
 
-def scale(X):
-    if not os.path.isfile(SCALER_PATH) or not os.path.isfile(VOLUME_SCALER_PATH):
-        make_scaler()
+# def inverse_scale(data, with_volume=True):
+#     if with_volume:
+#         scl: MinMaxScaler = pickle.load(open(SCALER_PATH, 'rb'))
+#         vscl: MinMaxScaler = pickle.load(open(VOLUME_SCALER_PATH, 'rb'))
+#         data[:, :-1] = scl.inverse_transform(data[:, :-1])
+#         data[:, -1:] = vscl.inverse_transform(data[:, -1:])
+#         return data
     
-    scl: MinMaxScaler = pickle.load(open(SCALER_PATH, 'rb'))
-    vscl: MinMaxScaler = pickle.load(open(VOLUME_SCALER_PATH, 'rb'))
-    X[:, -1:] = scl.transform(X[:, -1:])
-    X[:, :-1] = vscl.transform(X[:, :-1])
-    
-    return X
+#     scl: MinMaxScaler = pickle.load(open(SCALER_PATH, 'rb'))
+#     return scl.inverse_transform(data)
 
+
+# def scale(x):
+#     if not os.path.isfile(SCALER_PATH) or not os.path.isfile(SCALER_PATH):
+#         make_scaler()
+    
+#     scl: MinMaxScaler = pickle.load(open(SCALER_PATH, 'rb'))
+#     vscl: MinMaxScaler = pickle.load(open(VOLUME_SCALER_PATH, 'rb'))
+#     x[:, :-1] = scl.transform(x[:, :-1])
+#     x[:, -1:] = vscl.transform(x[:, -1:])
+#     return x
 
 def make_dataset(df: np.ndarray, time_d: int = 10):
     X, y = [], []
@@ -70,6 +112,7 @@ def make_dataset(df: np.ndarray, time_d: int = 10):
         
     return np.array(X), np.array(y)
 
+@cached(cache=CACHE)
 def get_loader_for_file(path: str, batch_size: int, time_d: int, as_double: bool = False):
     df = pd.read_csv(path)
     if len(df) < time_d:
@@ -77,12 +120,12 @@ def get_loader_for_file(path: str, batch_size: int, time_d: int, as_double: bool
     
     # remove date tag
     df.drop(['Date'], axis=1, inplace=True)
+    df = df[['Low', 'Open', 'High', 'Close', 'Adjusted Close', 'Volume']]
     
     # scale with min max scaler
-    df = pd.DataFrame(scale(df.to_numpy()), columns=['Low', 'Open', 'Volume', 'High', 'Close', 'Adjusted Close'])
+    df = pd.DataFrame(scale(torch.tensor(df.to_numpy())), columns=['Low', 'Open', 'High', 'Close', 'Adjusted Close', 'Volume'])
     
     # rearrange columns for 'make_dataset'
-    df = df[['Low', 'Open', 'High', 'Close', 'Adjusted Close', 'Volume']]
     
     # make dataset from dataframe
     X, y = make_dataset(df.to_numpy(), time_d)

lib/plots.py
@@ -1,4 +1,4 @@
-import os, torch, io
+import os, torch, io, sys
 import plotly.graph_objects as go
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,11 +7,19 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from PIL import Image
 from datetime import datetime
 
+from lib.data import inverse_scale
+
 RESULTS_PATH = os.path.join('results', datetime.now().strftime("%m-%d-%Y"))
 os.makedirs(RESULTS_PATH, exist_ok=True)
 
-def make_candle_plot(x, y, name: str = 'prediction', output_cols: int = 5):
-    data = torch.vstack((x[:, :output_cols], y.reshape(1, output_cols)))
+def make_candle_plot(x: torch.Tensor, y: torch.Tensor, name: str = 'prediction', output_cols: int = 5):
+    inp = x[:, :-1]
+    out = y.reshape(1, output_cols)
+
+    inp = inverse_scale(inp)
+    out = inverse_scale(out)
+
+    data = torch.vstack((inp, out))
 
     pred_x = len(x) - 0.5
     fig = go.Figure(data=[go.Candlestick(x=[i for i in range(len(x) + 1)],

train.py
@@ -13,10 +13,10 @@ from lib.data import get_all_csv_files, get_loader_for_file
 from lib.plots import make_color_gradient_compare_plot, make_compare_candle_plots
 
 DATA_PATH = './stock_market_data'
-FILES = get_all_csv_files(DATA_PATH)[:10]
+FILES = get_all_csv_files(DATA_PATH)[:100]
 EXCEPTIONS = []
 
-TIME_D = 10
+TIME_D = 14
 INPUT_LAYERS = 6
 OUTPUT_LAYERS = 5
 BATCH_SIZE = 512
@@ -27,8 +27,8 @@ AS_DOUBLE = False
 WAB = False
 N_RENDER_IMAGES = 2
 
-LOAD_MODEL = False
-LOAD_MODEL_PATH = os.path.join('models', 'sp500_413_loss_688.tar.pth')
+LOAD_MODEL = True
+LOAD_MODEL_PATH = os.path.join('models', 'checkpoint_epoch_9_07-14-2022.tar.pth')
 
 m = ConvLSTM(TIME_D, INPUT_LAYERS, OUTPUT_LAYERS).to(DEVICE)
 if AS_DOUBLE:
@@ -137,7 +137,7 @@ def train():
                 raise e
 
 
-        now = datetime.now().strftime("%m-%d-%Y")
+        now = datetime.now().strftime("%m-%d-%Y_%h-%M")
         torch.save(m.state_dict(), os.path.join('models', f'checkpoint_epoch_{epoch}_{now}.tar.pth'))
 
 if __name__ == '__main__':