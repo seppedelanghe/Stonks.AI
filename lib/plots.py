508af8260897d33c283c532d8f71ff7633803638
Seppe De Langhe <seppedelanghe@gmail.com>
2022-07-15 21:27:43 +0200

    add terminal loss plot

--- lib/plots.py
+++ lib/plots.py
@@ -8,9 +8,19 @@ from PIL import Image
 from datetime import datetime
 
 from lib.data import inverse_scale
+from lib.utils import unicode_block
 
 RESULTS_PATH = os.path.join('results', datetime.now().strftime("%m-%d-%Y"))
 os.makedirs(RESULTS_PATH, exist_ok=True)
+
+def plot_terminal_graph(x, name: str, max_size: int = 80):
+    if type(x) == torch.Tensor:
+        x = x.to_numpy()
+    
+    h = np.max(x)
+    idxs = np.rint(np.linspace(0, x.shape[0] - 1, max_size)).astype('int16')
+    line = ''.join([unicode_block(int(x[idx]/h*7)+1) for idx in idxs])
+    print(f"{name}:\t", line)
 
 def make_candle_plot(x: torch.Tensor, y: torch.Tensor, name: str = 'prediction', output_cols: int = 5):
     inp = x[:, :-1]
--- lib/utils.py
+++ lib/utils.py
@@ -6,3 +6,16 @@ def prod(val) :
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def unicode_block(s: int):
+    assert s > 0 and s < 9, 's to small or too large, needs to be 1-8'
+    return [
+        u'\u2581',
+        u'\u2582',
+        u'\u2583',
+        u'\u2584',
+        u'\u2585',
+        u'\u2586',
+        u'\u2587',
+        u'\u2588',
+    ][s-1]
